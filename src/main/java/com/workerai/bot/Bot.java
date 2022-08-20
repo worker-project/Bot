603ea0f53231e79308dcf634b4feae83d4161704
NoIdeaIndustry <noideaindustry@gmail.com>
2022-08-20 13:29:07 +0100

    🏗️ Renaming & gradle.build cleanup

--- src/main/java/com/workerai/bot/Bot.java
+++ src/main/java/com/workerai/bot/Bot.java
@@ -8,25 +8,24 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Bot {
-	private static JDA jda;
+    private static JDA jda;
 
-	public static void main(String[] args) throws Exception {
-		JDABuilder builder = JDABuilder.createDefault(args[0]);
-		builder.setActivity(Activity.playing("WorkerAI"));
-		builder.addEventListeners(new EventHandler());
-		builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT);
-		jda = builder.build();
+    public static void main(String[] args) throws Exception {
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+        builder.setActivity(Activity.playing("WorkerAI"));
+        builder.addEventListeners(new EventHandler());
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT);
+        jda = builder.build();
 
-		jda.awaitReady();
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> jda.shutdownNow()));
+        jda.awaitReady();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> jda.shutdownNow()));
 
-		jda.upsertCommand("send", "envoit le message pour créer un ticket dans le salon spécifié").addOption(OptionType.CHANNEL, "channel", "le salon dans lequel envoyer le message", true).queue();
-		jda.upsertCommand("ping", "Calculate ping of the bot").queue();
-		jda.upsertCommand("info", "Show info about the specified user").addOption(OptionType.USER, "user", "the user").queue();
-	}
+        jda.upsertCommand("send", "envoit le message pour créer un ticket dans le salon spécifié").addOption(OptionType.CHANNEL, "channel", "le salon dans lequel envoyer le message", true).queue();
+        jda.upsertCommand("ping", "Calculate ping of the bot").queue();
+        jda.upsertCommand("info", "Show info about the specified user").addOption(OptionType.USER, "user", "the user").queue();
+    }
 
-
-	public static JDA getJda() {
-		return jda;
-	}
+    public static JDA getJda() {
+        return jda;
+    }
 }
